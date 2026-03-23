eac9044fb807d728293dff94f439c0804e8e4c11
malakbzd <168239599+malakbzd@users.noreply.github.com>
2026-03-23 17:28:11 +0100

    front end / backend installation and frontend host

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
+// Replace with your friends' backend URL
 const API = "http://localhost:5000/api/tasks";
 
 export const getPatients = () => axios.get(API);
 export const addPatient = (data) => axios.post(API, data);
-export const updatePatient = (id, data) =>
-  axios.put(`${API}/${id}`, data);
-export const deletePatient = (id) =>
-  axios.delete(`${API}/${id}`);
+export const updatePatient = (id, data) => axios.put(`${API}/${id}`, data);
+export const deletePatient = (id) => axios.delete(`${API}/${id}`);
