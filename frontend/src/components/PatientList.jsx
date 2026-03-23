ebd8efac283fc2f15b904b81c3f858a9e2d5809e
malakbzd <168239599+malakbzd@users.noreply.github.com>
2026-03-23 21:38:58 +0100

    add

---
+++ frontend/src/components/PatientList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PatientItem from "./PatientItem";
+
+function PatientList({ patients, fetchPatients }) {
+  return (
+    <div>
+      {patients.map((p) => (
+        <PatientItem
+          key={p._id}
+          patient={p}
+          fetchPatients={fetchPatients}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default PatientList;  
+
