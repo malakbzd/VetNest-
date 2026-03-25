7cc1e393f35f45d93226e7f1404877e627cb5865
ayu200 <khadraoui.aya07@gmail.com>
2026-03-25 17:25:25 +0100

    up

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { 
+      type: String, 
+      required: true,
+      trim: true
+    },
+
+    email: { 
+      type: String, 
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    password: { 
+      type: String, 
+      required: true,
+      minlength: 6
+    },
+
+    // 🔥 مهم باش تفصل systems
+    role: {
+      type: String,
+      enum: ["admin", "doctor", "user"],
+      default: "user"
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
