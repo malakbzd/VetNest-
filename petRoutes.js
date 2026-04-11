3cb81af9ac7391fbf2d5afd5ff253fc096f0b6b1
ayu200 <khadraoui.aya07@gmail.com>
2026-04-11 15:30:43 +0100

    up

--- petRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { createPet, getPets, deletePet } = require("../controllers/petController");
-
-router.post("/", createPet);
-router.get("/", getPets);
-router.delete("/:id", deletePet);
-
-module.exports = router;
